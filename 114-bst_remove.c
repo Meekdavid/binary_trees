b3dc134aa72c171f89579ce52fe05bd040f7e8e1
David Mboko <98537263+Meekdavid@users.noreply.github.com>
2023-06-03 12:44:35 +0100

    Update 114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
-efficient efficien
+efficient efficie
  * successor - get the next successor i mean the min node in the right subtree
  * @node: tree to check
  * Return: the min value of this tree
